3d08c01b540022c4e27c117f525c0a4034d5ece1
smartapant <smartapant@gmail.com>
2018-05-28 14:28:24 +0300

    feat(posts list): infinite scroll UI

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,29 +1,53 @@
 <template>
-  <v-list class="posts-list" three-line>
-    <posts-list-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-    />
-  </v-list>
+  <div class="posts-list">
+    <v-list
+      v-infinite-scroll="loadMore"
+      infinite-scroll-distance="10"
+      infinite-scroll-disabled="loading"
+      three-line
+      v-show="posts.length"
+    >
+      <posts-list-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+      />
+    </v-list>
+   <posts-list-loader v-if="loading"/>
+  </div>
 </template>
 
 <script>
+  import infiniteScroll from 'vue-infinite-scroll'
   import PostsListItem from './PostsListItem.vue'
+  import PostsListLoader from './PostsListLoader.vue'
   export default {
     name: 'PostsList',
     components: {
-      PostsListItem
+      PostsListItem,
+      PostsListLoader
+    },
+    directives: {
+      infiniteScroll
     },
     props: {
       posts: {
         type: Array,
         default: []
+      },
+      loading: {
+        type: Boolean,
+        default: false
       }
     },
     data () {
       return {
 
+      }
+    },
+    methods: {
+      loadMore () {
+        console.log('load more!!!!')
       }
     }
   }
---
+++ src/components/PostsListLoader.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="posts-list-loader" fill-height>
+    <v-layout row wrap align-center>
+      <v-flex class="text-xs-center">
+        <v-progress-circular :size="50" indeterminate color="primary"></v-progress-circular>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'PostsListLoader'
+  }
+</script>
+
+<style>
+
+</style>
