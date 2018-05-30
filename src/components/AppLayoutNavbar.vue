7f2d89ff91684bb430c6e601e3dc583b7142b1f8
smartapant <smartapant@gmail.com>
2018-05-30 19:53:07 +0300

    feat(navbar): subreddit title

--- src/components/AppLayoutNavbar.vue
+++ src/components/AppLayoutNavbar.vue
@@ -5,17 +5,27 @@
     color="primary"
   >
     <v-toolbar-side-icon @click.stop="changeSidebarState"></v-toolbar-side-icon>
-    <v-toolbar-title>Vue Reddit PWA</v-toolbar-title>
+    <v-toolbar-title>Vue Reddit PWA {{subredditTitle}}</v-toolbar-title>
   </v-toolbar>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
     name: 'AppLayoutNavbar',
     props: {
       sidebarShownState: {
         type: Boolean,
         default: null
+      }
+    },
+    computed: {
+      ...mapGetters('subreddits', [
+        'selectedSubreddit'
+      ]),
+      subredditTitle () {
+        return this.selectedSubreddit ? `- ${this.selectedSubreddit}` : ''
       }
     },
     methods: {
